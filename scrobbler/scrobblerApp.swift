8ac8f50a5a1f6a6a352b8a318cc2f3d7216bb9e1
Fabián Sanhueza <sanhuesoft@gmail.com>
2026-03-29 20:09:16 -0300

    Initial Commit

---
+++ scrobbler/scrobblerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrobblerApp.swift
+//  scrobbler
+//
+//  Created by Fabián Sanhueza on 29-03-26.
+//
+
+import SwiftUI
+
+@main
+struct scrobblerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
